65d59f107bc875ff3becc50a8a9eae074fce268c
Thallys <bezerra.thallys@gmail.com>
2022-11-14 21:24:16 -0300

    fixes placeholder

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,27 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import moment from 'moment';
-import type {NextPage} from 'next';
+import type { NextPage } from 'next';
 import { Task } from '../types/Task';
 
 type ItemProps = {
     task: Task,
-    selectTask(task:Task) :void
+    selectTask(task: Task): void
 }
 
-export const Item : NextPage<ItemProps> = ({task, selectTask}) =>{
+export const Item: NextPage<ItemProps> = ({ task, selectTask }) => {
     const isTaskFinished = task.finishDate || false;
 
-    const getDateText = (finishDate: Date | undefined, finishPrevisionDate : Date) => {
-        if(finishDate){
-            return `Concluído em: ${moment(finishDate).format('DD/MM/yyyy')}`;
+    const getDateText = (finishDate: Date | undefined, finishPrevisionDate: Date) => {
+        if (finishDate) {
+            return `Concluído em: ${moment.utc(finishDate).format('DD/MM/yyyy')}`;
         }
-        return `Conclusão em: ${moment(finishPrevisionDate).format('DD/MM/yyyy')}`;
+        return `Conclusão em: ${moment.utc(finishPrevisionDate).format('DD/MM/yyyy')}`;
     }
 
     return (
         <div className={'container-item' + (isTaskFinished ? '' : ' active')}
             onClick={e => isTaskFinished ? null : selectTask(task)}>
-            <img src={isTaskFinished ? 'checked.svg' : 'not-checked.svg'} alt={isTaskFinished ? 'Tarefa em aberto' : 'Tarefa concluída'}/>
+            <img src={isTaskFinished ? 'checked.svg' : 'not-checked.svg'} alt={isTaskFinished ? 'Tarefa em aberto' : 'Tarefa concluída'} />
             <div>
                 <p className={isTaskFinished ? 'finished' : ''}>{task.name}</p>
                 <span>{getDateText(task.finishDate, task.finishPrevisionDate)}</span>
